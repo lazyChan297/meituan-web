e4eef67cd1c7bcdfe6f207b68c52b61cc6bacd11
lazyChan297 <lazyChan297@163.com>
2018-10-18 18:14:49 +0800

    10.18

--- src/main.js
+++ src/main.js
@@ -3,11 +3,32 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
-
-// 引入公共css
-import './assets/style/common.scss'
+import axios from 'axios'
+import './assets/style/test.css'
 Vue.config.productionTip = false
+axios.defaults.baseURL = 'http://127.0.0.1:8888'
+axios.interceptors.request.use(
+    config => {
+        console.log(config)
+        return config;
+    },
+    err => {
 
+    });
+axios.interceptors.response.use(
+  response => {
+    if(response.data.code===1){
+      return response.data.retObj
+    }else {
+      return {error:'error'}
+    }
+  },
+  error => {
+    console.log(error)
+  }
+)
+
+Vue.prototype.axios = axios;
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
